40bbf9f35623a83a6e11d8fbb2438db97ed01a8a
milank-s <mks554@nyu.edu>
2016-12-05 22:31:23 -0500

    Tree Leaves Added

--- Assets/MyScript/LeafBehaviour.cs
+++ Assets/MyScript/LeafBehaviour.cs
@@ -3,20 +3,25 @@ using System.Collections;
 
 public class LeafBehaviour : MonoBehaviour {
 
+	bool activated = false;
+	float offset;
 	// Use this for initialization
 	void Start () {
-	
+		offset = Random.Range(0.0f, 2 * Mathf.PI);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (activated) {
+			GetComponent<Rigidbody2D> ().AddTorque (Mathf.Cos (offset + Time.deltaTime * 100) * 5);
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (Mathf.Sin (Time.deltaTime), 0));
+		}
 	}
 
 	public void OnTriggerEnter2D(Collider2D col){
 		if(col.gameObject.tag == "Player"){
 			GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.None;
-			gameObject.layer = 12;
+			gameObject.layer = 11;
 		}
 	}
 }
---
+++ Assets/MyScript/LeafSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeafSpawner : MonoBehaviour {
+
+	public GameObject leaf;
+	public int amount;
+	// Use this for initialization
+	void Start () {
+		SpawnLeaves ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnLeaves(){
+		for(int i = 0; i < amount; i++){
+			Vector2 range = Random.insideUnitCircle * GetComponent<Collider2D> ().bounds.extents.y;
+			Vector3 pos = new Vector3 (transform.position.x + range.x, transform.position.y + range.y, 0);
+			if(GetComponent<Collider2D>().OverlapPoint(pos)){
+				GameObject newLeaf = (GameObject)Instantiate (leaf, pos, Quaternion.identity);
+				newLeaf.transform.parent = transform;
+				newLeaf.transform.Rotate (0, 0, Random.Range (-90, 90));
+			}
+		}
+	}
+}
